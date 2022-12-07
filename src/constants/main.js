72f7dd8086d97b5d4fd4e5fca7eac0e6d5fc3ec6
roclimber <roclimber1@gmail.com>
2022-12-07 08:10:46 +0300

    Initial commit

---
+++ src/constants/main.js
@@ -0,0 +1,7 @@
+export const TOTAL_CHARACTERS = 826;
+
+export const URLS_LIST = {
+  HOME: "/",
+  LIST: "characters",
+  DETAILS: ":id"
+};
---
+++ src/pages/RandomCharacter.js
@@ -0,0 +1,51 @@
+// import React from 'react'
+
+// import { ItemWrapper } from '../components/Wrapper'
+
+// import CharacterCard from '../components/CharacterCard'
+// import { Layout } from '../components/RoutesLayout'
+
+// import Button from '../components/Button'
+
+// import { getCharacter, getRandomId } from '../services/api'
+
+// import useRequestProcessor from '../hooks/useRequestProcessor'
+
+// const Home = () => {
+
+//     const [id, setId] = React.useState(getRandomId())
+
+//     const data = useRequestProcessor({ requestData: getCharacter, parameters: id })
+
+//     React.useEffect(() => {
+
+//         setId(getRandomId())
+//     }, [])
+
+//     const handleClick = () => {
+
+//         setId(getRandomId())
+//     }
+
+//     return (<React.Fragment>
+
+//         <Layout>
+
+//             <ItemWrapper>
+
+//                 <CharacterCard {...data} />
+
+//             </ItemWrapper>
+
+//             <ItemWrapper>
+
+//                 <Button title={'Update'} onClick={handleClick} />
+
+//             </ItemWrapper>
+
+//         </Layout>
+
+//     </React.Fragment>)
+// }
+
+// export default Home
